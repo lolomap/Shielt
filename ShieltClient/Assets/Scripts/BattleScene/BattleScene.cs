04e00829f089a32580e2e147861f06f514412537
Azorious07 <133042335+Azorious07@users.noreply.github.com>
2024-11-24 00:57:45 +0300

    Modal after battle menu

--- ShieltClient/Assets/Scripts/BattleScene/BattleScene.cs
+++ ShieltClient/Assets/Scripts/BattleScene/BattleScene.cs
@@ -1,47 +1,79 @@
+using System;
+using TMPro;
 using UnityEngine;
+using UnityEngine.UIElements;
 
 namespace BattleScene
 {
-   public class BattleScene : MonoBehaviour
-   {
-      public HpBarManager HealthBar1, HealthBar2;
-      public PlayerScript Player1, Player2;
-      
-      private void Start()
-      {
-         Network.Instance.Connect();
-         EmotionalPage.Instance.StartScanning();
-         
-         Network.UpdatePlayers += (players) =>
-         {
-            HealthBar1.SetHp(players.Player1Health);
-            HealthBar2.SetHp(players.Player2Health);
+	public class BattleScene : MonoBehaviour
+	{
+		public HpBarManager HealthBar1, HealthBar2;
+		public PlayerScript Player1, Player2;
+		public TMP_Text WinnerText;
+		public GameObject EndRoundPanel;
+		private void Start()
+		{
+            Network.Instance.Connect();
+			EmotionalPage.Instance.StartScanning();
 
-            bool isFinish = false;
-            if (players.Player1Health <= 0)
-            {
-               Player1.Die();
-               isFinish = true;
-            }
-            if (players.Player2Health <= 0)
-            {
-               Player2.Die();
-               isFinish = true;
-            }
+			Network.UpdatePlayers += (players) =>
+			{
+				HealthBar1.SetHp(players.Player1Health);
+				HealthBar2.SetHp(players.Player2Health);
 
-            if (isFinish)
-            {
-               // MODAL
-            }
-            
-            if (players.Player1IsDefend)
-                 Player1.Defend(); 
-             else Player1.Attack();
-            if (players.Player2IsDefend)
-                 Player2.Defend(); 
-             else Player2.Attack();
-         };
-         
-      }
-   }
+                String Winner = "";
+                bool isFinish = false;
+				if (players.Player1Health <= 0)
+				{
+					Player1.Die();
+					Winner = "Player 2";
+					isFinish = true;
+				}
+				if (players.Player2Health <= 0)
+				{
+					Player2.Die();
+					if (Winner == "Player 2")
+						Winner = "Draw";
+					else
+						Winner = "Player 1";
+					isFinish = true;
+				}
+
+				if (isFinish)
+				{
+					DisplayWinner(Winner);
+					EndRoundPanel.SetActive(true);
+				}
+
+				if (players.Player1IsDefend)
+					Player1.Defend();
+				else Player1.Attack();
+				if (players.Player2IsDefend)
+					Player2.Defend();
+				else
+					Player2.Attack();
+			};
+
+		}
+
+		public void DisplayWinner(String winner)
+		{
+			WinnerText.text = $"The winner is: + \n + {winner}!";
+		}
+
+		public void ExitGame()
+		{
+			Application.Quit();
+		}
+
+		public void ResetBattle()
+		{
+			HealthBar1.SetHp(100);
+            HealthBar1.SetHp(100);
+			Player1.Idle();
+			Player2.Idle();
+			EndRoundPanel.SetActive(false);
+			//Special server package
+        }
+	}
 }
